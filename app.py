1ce5fa19518b1c631f17b6db44c855345d6e9fcb
Luan Kuwabara <luankuwabara@gmail.com>
2025-09-18 23:40:07 -0300

    fix: connection refused with mongo, reordering

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "users",
+        "host": "mongo",
+        "port": 27017,
+        "username": "admin",
+        "password": "admin",
+        "alias": "default",
+    }
+]
 api = Api(app)
+db = MongoEngine()
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.EmailField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
+
+
+db = MongoEngine(app)
+
 
 class Users(Resource):
     def get(self):
         return {'message': 'user 1'}
+
 
 class User(Resource):
     def post(self):
@@ -14,6 +40,7 @@ class User(Resource):
 
     def get(self, cpf):
         return {'message': 'CPF'}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
