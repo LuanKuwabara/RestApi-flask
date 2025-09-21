b779ab21cbbadafe147de33395f555c8e5deb590
Luan Kuwabara <luankuwabara@gmail.com>
2025-09-21 19:08:50 -0300

    fix: NotUniqueError cpf inserting

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
+from mongoengine import NotUniqueError
+import re
 
 app = Flask(__name__)
 
@@ -19,11 +21,27 @@ api = Api(app)
 db = MongoEngine(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('first_name', type=str, required=True, help="This field cannot be blank")
-parser.add_argument('last_name', type=str, required=True, help="This field cannot be blank")
-parser.add_argument('cpf', type=str, required=True, help="This field cannot be blank")
-parser.add_argument('email', type=str, required=True, help="This field cannot be blank")
-parser.add_argument('birth_date', type=str, required=True, help="This field cannot be blank")
+parser.add_argument('first_name',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank")
+parser.add_argument('last_name',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank")
+parser.add_argument('cpf',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank")
+parser.add_argument('email',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank")
+parser.add_argument('birth_date',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank")
+
 
 class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
@@ -32,17 +50,39 @@ class UserModel(db.Document):
     last_name = db.StringField(required=True)
     birth_date = db.DateTimeField(required=True)
 
+
+def validar_cpf(cpf):
+    cpf = re.sub(r'[^0-9]', '', cpf)
+    if len(cpf) != 11 or cpf == cpf[0] * 11:
+        return False
+    for i in range(9, 11):
+        soma = sum(int(cpf[num]) * ((i+1) - num) for num in range(0, i))
+        digito = ((soma * 10) % 11) % 10
+        if int(cpf[i]) != digito:
+            return False
+    return True
+
 class Users(Resource):
     def get(self):
         return {'message': 'user 1'}
 
+
 class User(Resource):
     def post(self):
         data = parser.parse_args()
-        UserModel(**data).save()
+        if not validar_cpf(data['cpf']):
+            return {'message': 'CPF Inválido'}, 400
+        
+        try:
+            response = UserModel(**data).save()
+            return {'message': f"Usuário {response} criado com sucesso!"}, 201
+        except NotUniqueError:
+            return {"message": "CPF já existente!"}, 400
+
 
     def get(self, cpf):
         return {'message': f'User with CPF {cpf}'}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
